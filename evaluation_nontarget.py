e1145f73ec6a1a499482d814a280bc5cb355a292
Chrisqcwx <3075401625@qq.com>
2024-06-15 19:31:02 +0800

    feat: non target eval

--- evaluation_nontarget.py
+++ evaluation_nontarget.py
@@ -38,7 +38,9 @@ def compute_ap(precision, recall):
 @torch.no_grad()
 def evaluation_nontarget(mode, iou_thresholds=default_iou_thresholds):
 
-    dataset = ClassificationDataset(hyp.anno_path, hyp.raw_dataset_dir, mode=mode)
+    dataset = ClassificationDataset(
+        hyp.anno_path, hyp.raw_dataset_dir, mode=mode, labeled=False
+    )
     detector = Detector(hyp.lownet_path, hyp.midnet_path, hyp.highnet_path, hyp.device)
 
     # if use_classifier:
@@ -74,82 +76,19 @@ def evaluation_nontarget(mode, iou_thresholds=default_iou_thresholds):
     return precision, recall, f1, ap, meaniou
 
 
-@torch.no_grad()
-def evaluation_target(mode, iou_thresholds=default_iou_thresholds):
-
-    dataset = ClassificationDataset(hyp.anno_path, hyp.raw_dataset_dir, mode=mode)
-    detector = Detector(hyp.lownet_path, hyp.midnet_path, hyp.highnet_path, hyp.device)
-
-    # if use_classifier:
-    classifier = auto_classifier_from_pretrained(hyp.ir152_save_path).to(hyp.device)
-    classifier.eval()
-
-    # print(len(dataset.classes))
-    TP = np.zeros((len(dataset.classes), len(iou_thresholds)))
-    FP = np.zeros((len(dataset.classes), len(iou_thresholds)))
-
-    ALL = np.zeros((len(dataset.classes),))
-
-    all_iou = []
-
-    for img, target, label_box in tqdm(dataset):
-        pred_boxes = detector.detect(img)
-        if len(pred_boxes) == 0:
-            # FP += 1
-            continue
-        # print(boxutils.iou(label_box, pred_boxes))
-        # exit()
-        ious = boxutils.iou(label_box, pred_boxes)
-        ALL[target] += 1
-        for box, iou in zip(pred_boxes, ious):
-            x1 = int(box[0])
-            y1 = int(box[1])
-            x2 = int(box[2])
-            y2 = int(box[3])
-            cls = box[4]
-
-            # if use_classifier:
-            newim = img.crop((x1, y1, x2, y2))
-            newim_tensor = trans(newim).unsqueeze(0).to(hyp.device)
-            pred = classifier(newim_tensor)[0].argmax(dim=-1).cpu().item()
-            mask = (iou_thresholds >= iou) & (pred == target)
-
-            all_iou.append(iou)
-            # print(TP.shape, pred)
-            TP[pred][mask] += 1
-            FP[pred][~mask] += 1
-            break
-
-    precision = TP / (TP + FP)
-    precision = np.where(TP + FP == 0, 0, precision)
-    recall = TP / ALL.reshape(-1, 1)
-    eps = 1e-7
-    f1 = 2 / (
-        1 / precision.clip(min=eps, max=None) + 1 / recall.clip(min=eps, max=None)
-    )
-
-    aps = []
-    for i in range(len(precision)):
-        aps.append(compute_ap(precision[i], recall[i]))
-    aps = np.array(aps).mean()
-    meaniou = np.array(all_iou).mean()
-
-    return precision.mean(axis=0), recall.mean(axis=0), f1.mean(axis=0), aps, meaniou
-
-
 if __name__ == '__main__':
-    # precision, recall, f1, ap, meaniou = evaluation_nontarget('val')
-    # print(ap, meaniou)
-    # os.makedirs('./results_eval')
-    # torch.save([precision, recall, f1, ap, meaniou], 'val_nontarget.pt')
+    precision, recall, f1, ap, meaniou = evaluation_nontarget('val')
+    print(ap, meaniou)
+    os.makedirs('./results_eval')
+    torch.save([precision, recall, f1, ap, meaniou], 'val_nontarget.pt')
     # precision, recall, f1, ap, meaniou = evaluation_target('val')
     # print(ap, meaniou)
     # os.makedirs('./results_eval', exist_ok=True)
     # torch.save([precision, recall, f1, ap, meaniou], 'val_target.pt')
 
-    # precision, recall, f1, ap, meaniou = evaluation_nontarget('test')
-    # print(ap, meaniou)
-    # torch.save([precision, recall, f1, ap, meaniou], 'test_nontarget.pt')
+    precision, recall, f1, ap, meaniou = evaluation_nontarget('test')
+    print(ap, meaniou)
+    torch.save([precision, recall, f1, ap, meaniou], 'test_nontarget.pt')
     # precision, recall, f1, ap, meaniou = evaluation_target('test')
     # print(ap, meaniou)
     # torch.save([precision, recall, f1, ap, meaniou], 'test_target.pt')
